15981bac7b7358642c29691baca70bcd71e5b92e
Cypher012 <ayoojoade@gmail.com>
2025-09-09 19:10:01 +0100

    feat: enhance course management with create and update functions
    - Refactored the course interface to include an `id` property.
    - Updated the `createCourse` function to remove unnecessary generic type and ensure department names are strings.
    - Added a new `updateCourse` function for modifying existing courses, including error handling and redirection after updates.

--- src/lib/actions/courses.ts
+++ src/lib/actions/courses.ts
@@ -6,7 +6,8 @@ import { redirect } from "next/navigation";
 import type { Course as $Course, Department, Lecturer } from "~/generated/prisma";
 
 
-interface CreateCourse{
+export interface Course{
+    id: string
     course_code: string;
     course_title: string;
     departments: Department[];
@@ -22,7 +23,7 @@ export type CourseWithRelations = {
 } & $Course
 
 // CREATE
-export async function createCourse<CreateCourse>(formData: FormData) {
+export async function createCourse(formData: FormData) {
   const course_code = String(formData.get("code") || "").trim();
   const course_title = String(formData.get("title") || "").trim();
   const departments = Array(formData.get("departments") || "") || null;
@@ -31,10 +32,37 @@ export async function createCourse<CreateCourse>(formData: FormData) {
         await db.course.create({
             data: {course_code, course_title,
                 departments: {
-                    connect: departments.map((name) => ({name}))
-                }
+                    connect: departments.map((name) => ({ name: String(name) }))
+                  }
              }
         })
     }catch (err) {}
 
 }
+
+
+// UPDATE
+export async function updateCourse(id: string, formData: FormData) {
+    try {
+      const course_code = formData.get("course_code") as string | null;
+      const course_title = formData.get("course_title") as string | null;
+  
+      if (!course_code || !course_title) {
+        throw new Error("course_code and course_title are required");
+      }
+  
+      await db.course.update({
+        where: { id },
+        data: { course_code, course_title },
+      });
+  
+      redirect("/admin/courses")
+    } catch (error) {
+      console.error("Error updating department:", error);
+      redirect(`/admin/courses?error=${encodeURIComponent((error as Error).message)}`)
+    } finally{
+      revalidatePath("/admin/courses")
+    }
+  
+  }
+  
